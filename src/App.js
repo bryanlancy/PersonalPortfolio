76667b0110cdec66fb95908592dec52ae0ac2f5a
Bryan Burns <bryanburns93@gmail.com>
2021-06-06 14:30:13 -0400

    remove extraneous files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { Route, Switch } from 'react-router-dom'
 
 import HomePage from './components/pages/Home'
-import Navigation from './components/Navigation'
 
 function App() {
 	const [isLoaded, setIsLoaded] = useState(false)
@@ -10,17 +8,7 @@ function App() {
 		setIsLoaded(true)
 	}, [])
 
-	return (
-		<>
-			{isLoaded && (
-				<Switch>
-					<Route path="/">
-						<HomePage />
-					</Route>
-				</Switch>
-			)}
-		</>
-	)
+	return <>{isLoaded && <HomePage />}</>
 }
 
 export default App
