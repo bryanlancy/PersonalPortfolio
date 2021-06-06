36506b2417b6c04315b0597c8804e270e4d89eea
Bryan Burns <bryanburns93@gmail.com>
2021-06-06 14:32:00 -0400

    remove extraneous files

--- src/index.js
+++ src/index.js
@@ -9,16 +9,11 @@ import { ModalProvider } from './context/Modal'
 import App from './App'
 
 import configureStore from './store'
-import { restoreCSRF, fetch } from './store/csrf'
-import * as sessionActions from './store/session'
 
 const store = configureStore()
 
 if (process.env.NODE_ENV !== 'production') {
-	restoreCSRF()
-	window.csrfFetch = fetch
 	window.store = store
-	window.sessionActions = sessionActions
 }
 
 function Root() {
