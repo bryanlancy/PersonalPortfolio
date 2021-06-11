3f971029d623ba88fefdca5e4a80e98a753146ae
Bryan Burns <bryanburns93@gmail.com>
2021-06-11 03:42:59 -0400

    fix images

--- src/components/pages/Home/sections/Projects/ProjectContainer/index.js
+++ src/components/pages/Home/sections/Projects/ProjectContainer/index.js
@@ -6,13 +6,14 @@ export default function ProjectContainer({ image, icon, title, sub, desc, left }
 	const textShift = '15%'
 	const defaultTextStyle = { [!left ? 'left' : 'right']: textShift }
 	const [textStyle, setTextStyle] = useState(defaultTextStyle)
+
 	function hoverStyle(e) {
 		e.type === 'mouseenter' ? setTextStyle({ [!left ? 'left' : 'right']: textShift, transform: `translateX(${!left ? '-' : ''}20vw)`, color: '#fff' }) : setTextStyle(defaultTextStyle)
 	}
 
 	return (
 		<div className="project" onMouseEnter={hoverStyle} onMouseLeave={hoverStyle}>
-			<img className="project__image" src={`images/${image}`} alt={`${title}-project`} style={{ [left ? 'left' : 'right']: '-24px' }} />
+			<img className="project__image" src={require(`../images/${image}`).default} alt={`${title}-project`} style={{ [left ? 'left' : 'right']: '-24px' }} />
 			<div className="project__text" style={textStyle}>
 				<div className="project__text--title">
 					{icon} <b>{title}</b>
