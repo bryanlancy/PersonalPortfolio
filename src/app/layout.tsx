3a12f80d7088e27bcb368fa1c2b758d3d439e277
bryanlancy <bryanburns93@gmail.com>
2024-08-28 03:26:45 -0400

    add font awesome fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from 'next'
 import { Bebas_Neue } from 'next/font/google'
 import './globals.scss'
 
+// Fix Font Awesome icons
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 // Display/title fonts
 const bebasNeue = Bebas_Neue({ subsets: ['latin'], weight: '400' })
 
