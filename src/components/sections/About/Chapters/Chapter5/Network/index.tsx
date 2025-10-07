142a27f2e94a9883b3482cc36ec63a501ab4e785
bryanlancy <bryanburns93@gmail.com>
2025-10-07 14:20:38 -0400

    finish network, so bad :(

--- src/components/sections/About/Chapters/Chapter5/Network/index.tsx
+++ src/components/sections/About/Chapters/Chapter5/Network/index.tsx
@@ -1,4 +1,4 @@
-import { RefObject, useRef, useState } from 'react'
+import { RefObject, useEffect, useMemo, useRef, useState } from 'react'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
@@ -28,7 +28,7 @@ interface Node extends Coord {
 	className: string
 	icon: FontAwesomeIconProps['icon']
 	order: number
-	binaryRef?: RefObject<GSAPTimeline | null>
+	binaryRef: GSAPTimeline | null
 }
 
 const Network = () => {
@@ -39,84 +39,9 @@ const Network = () => {
 	const serverDataPathRef = useRef<SVGPathElement>(null)
 	const serverTabletPathRef = useRef<SVGPathElement>(null)
 
-	const binary1TlRef = useRef<GSAPTimeline | null>(null)
-	const binary2TlRef = useRef<GSAPTimeline | null>(null)
-	const binary3TlRef = useRef<GSAPTimeline | null>(null)
-	const binary4TlRef = useRef<GSAPTimeline | null>(null)
-
-	const [svgDimensions, setSvgDimensions] = useState<[number, number]>([
-		500, 100,
-	])
-	function scaleDimensions(x: number, y: number): [number, number] {
-		const cx = (x / 100) * svgDimensions[0]
-		const cy = (y / 100) * svgDimensions[1]
-		return [cx, cy]
-	}
-	function pointsToPath(points: Node[], curviness = 0) {
-		const pathString = MotionPathPlugin.rawPathToString(
-			MotionPathPlugin.arrayToRawPath(
-				points.map(p => {
-					const [x, y] = scaleDimensions(p.x, p.y)
-					return { x, y: y - y * 0.07 }
-				}),
-				{ curviness }
-			)
-		)
-
-		return pathString
-	}
-
 	const { contextSafe } = useGSAP({
 		dependencies: [],
 	})
-
-	const showNode = contextSafe(
-		(
-			className: string | string[],
-			offset: number,
-			duration: number,
-			binaryTlRef?: RefObject<GSAPTimeline | null>
-		) => {
-			const nodeTl = gsap.timeline({
-				scrollTrigger: {
-					trigger: '.chapter5',
-					start: `top top-=${offset}`,
-					end: `+=${duration}px`,
-					scrub: true,
-				},
-				onUpdate: self => {
-					console.log(binaryTlRef?.current)
-					if (binaryTlRef?.current) {
-						console.log(self.progress)
-						if (self.direction > 0 && self.progress >= 0.95) {
-							binaryTlRef.current.play()
-						} else {
-							binaryTlRef.current.pause(0)
-						}
-					}
-				},
-			})
-			nodeTl.to(className, {
-				autoAlpha: 1,
-				duration: 0.1,
-			})
-			nodeTl.to(className, {
-				boxShadow:
-					'inset 5px 5px 10px #140025, inset -5px -5px 10px #500093',
-				duration: 1,
-			})
-			nodeTl.to(
-				`${className} .${styles.icon}`,
-				{
-					autoAlpha: 1,
-					delay: 0.2,
-					duration: 1,
-				},
-				'<'
-			)
-			return nodeTl
-		}
-	)
 
 	const makeBinaryPath = contextSafe(
 		(
@@ -168,6 +93,84 @@ const Network = () => {
 		}
 	)
 
+	// Light to computer
+	const binary1Tl = useRef<GSAPTimeline | null>(null)
+	const binary2Tl = useRef<GSAPTimeline | null>(null)
+	const binary3Tl = useRef<GSAPTimeline | null>(null)
+	const binary4Tl = useRef<GSAPTimeline | null>(null)
+
+	const [svgDimensions, setSvgDimensions] = useState<[number, number]>([
+		500, 100,
+	])
+
+	const showNode = contextSafe(
+		(
+			className: string | string[],
+			offset: number,
+			duration: number,
+			binaryTl: GSAPTimeline | null
+		) => {
+			const nodeTl = gsap.timeline({
+				scrollTrigger: {
+					trigger: '.chapter5',
+					start: `top top-=${offset}`,
+					end: `+=${duration}px`,
+					scrub: true,
+					onUpdate: self => {
+						console.log(className, binaryTl)
+
+						if (binaryTl) {
+							console.log(self.progress)
+							if (self.direction > 0 && self.progress >= 0.95) {
+								binaryTl.play()
+							} else {
+								binaryTl.pause(0)
+							}
+						}
+					},
+				},
+			})
+			nodeTl.to(className, {
+				autoAlpha: 1,
+				duration: 0.1,
+			})
+			nodeTl.to(className, {
+				boxShadow:
+					'inset 5px 5px 10px #140025, inset -5px -5px 10px #500093',
+				duration: 1,
+			})
+			nodeTl.to(
+				`${className} .${styles.icon}`,
+				{
+					autoAlpha: 1,
+					delay: 0.2,
+					duration: 1,
+				},
+				'<'
+			)
+			return nodeTl
+		}
+	)
+
+	function scaleDimensions(x: number, y: number): [number, number] {
+		const cx = (x / 100) * svgDimensions[0]
+		const cy = (y / 100) * svgDimensions[1]
+		return [cx, cy]
+	}
+	function pointsToPath(points: Node[], curviness = 0) {
+		const pathString = MotionPathPlugin.rawPathToString(
+			MotionPathPlugin.arrayToRawPath(
+				points.map(p => {
+					const [x, y] = scaleDimensions(p.x, p.y)
+					return { x, y: y - y * 0.07 }
+				}),
+				{ curviness }
+			)
+		)
+
+		return pathString
+	}
+
 	const ground = 65
 	const nodes: { [node: string]: Node } = {
 		computer: {
@@ -176,7 +179,7 @@ const Network = () => {
 			order: 1,
 			className: styles.computer,
 			icon: faComputerClassic,
-			binaryRef: binary1TlRef,
+			binaryRef: binary1Tl.current,
 		},
 		server: {
 			x: 65,
@@ -184,7 +187,7 @@ const Network = () => {
 			order: 2,
 			className: styles.server,
 			icon: faServer,
-			binaryRef: binary2TlRef,
+			binaryRef: binary2Tl.current,
 		},
 		database: {
 			x: 80,
@@ -192,7 +195,7 @@ const Network = () => {
 			order: 3,
 			className: styles.database,
 			icon: faDatabase,
-			binaryRef: binary3TlRef,
+			binaryRef: binary3Tl.current,
 		},
 		tower: {
 			x: 65,
@@ -200,6 +203,7 @@ const Network = () => {
 			order: 4,
 			className: styles.tower,
 			icon: faTowerBroadcast,
+			binaryRef: null,
 		},
 		satellite: {
 			x: 15,
@@ -207,6 +211,7 @@ const Network = () => {
 			order: 5,
 			className: styles.satellite,
 			icon: faSatellite,
+			binaryRef: null,
 		},
 		tablet: {
 			x: 15,
@@ -214,41 +219,11 @@ const Network = () => {
 			order: 6,
 			className: styles.tablet,
 			icon: faTabletScreenButton,
-			binaryRef: binary4TlRef,
+			binaryRef: binary4Tl.current,
 		},
 	}
 
 	useGSAP(() => {
-		// Light to computer
-		const path1 = makeBinaryPath(lightPathRef, `.${styles.lightBinary}`)
-		if (path1) {
-			binary1TlRef.current = path1
-		}
-		// Computer to server
-		const path2 = makeBinaryPath(
-			pcServerPathRef,
-			`.${styles.computerBinary}`
-		)
-		if (path2) {
-			binary2TlRef.current = path2
-		}
-		// Server to database
-		const path3 = makeBinaryPath(serverDataPathRef, `.${styles.dataBinary}`)
-		if (path3) {
-			binary3TlRef.current = path3
-		}
-		// Server to tablet
-		const path4 = makeBinaryPath(
-			serverTabletPathRef,
-			`.${styles.tabletBinary1}`,
-			{
-				duration: 3,
-			}
-		)
-		if (path4) {
-			binary4TlRef.current = path4
-		}
-
 		//
 		if (containerRef.current) {
 			const rect = containerRef.current?.getBoundingClientRect()
@@ -266,22 +241,24 @@ const Network = () => {
 			return `.${node.className}`
 		})
 
-		const nodeTl = gsap.timeline({
+		// Set auto alpha of nodes when out of section
+		const nodeAlphaTl = gsap.timeline({
 			scrollTrigger: {
 				trigger: '.chapter5',
 				start: 'top top-=999px',
 				end: '+=1px',
 				onUpdate: self => {
-					nodeTl.reversed(self.direction > 0 ? false : true)
+					nodeAlphaTl.reversed(self.direction > 0 ? false : true)
 				},
 			},
 		})
-		nodeTl.to(`.${styles.node}`, {
+		nodeAlphaTl.to(`.${styles.node}`, {
 			autoAlpha: 1,
-			duration: 0.1,
+			duration: 0.2,
 			stagger: 0,
 		})
 
+		// Add background behind lightbulb
 		const lightNodeTl = gsap.timeline({
 			scrollTrigger: {
 				trigger: '.chapter5',
@@ -301,56 +278,235 @@ const Network = () => {
 			boxShadow: '4px 4px 8px 0px #0008',
 		})
 
-		const networkTl = gsap.timeline({
+		//! dream, ref doesnt work as is, change how its passed?
+		// for (let i = 0; i < sortedNodes.length; i++) {
+		// const node = sortedNodes[i]
+		// 	showNode(
+		// 		`.${node.className}`,
+		// 		750 + i * 400,
+		// 		400,
+		// 		node.binaryRef
+		// 	)
+		// }
+
+		// Light to computer
+		const path1 = makeBinaryPath(lightPathRef, `.${styles.lightBinary}`)
+		if (path1) binary1Tl.current = path1
+		const node1Tl = gsap.timeline({
 			scrollTrigger: {
 				trigger: '.chapter5',
-				start: 'top top-=1000px',
-
-				end: '+=1750px',
-				// scrub: true,
+				start: `top top-=${750}`,
+				end: `+=${400}px`,
+				scrub: true,
+				onUpdate: self => {
+					if (binary1Tl.current) {
+						if (self.direction > 0 && self.progress >= 0.95) {
+							binary1Tl.current.play()
+						} else {
+							binary1Tl.current.pause(0)
+						}
+					}
+				},
 			},
 		})
+		node1Tl.to(nodeClasses[0], {
+			autoAlpha: 1,
+			duration: 0.1,
+		})
+		node1Tl.to(nodeClasses[0], {
+			boxShadow:
+				'inset 5px 5px 10px #140025, inset -5px -5px 10px #500093',
+			duration: 1,
+		})
+		node1Tl.to(
+			`${nodeClasses[0]} .${styles.icon}`,
+			{
+				autoAlpha: 1,
+				delay: 0.2,
+				duration: 1,
+			},
+			'<'
+		)
 
-		for (let i = 0; i < nodeClasses.length; i++) {
-			const node = nodeClasses[i]
-			networkTl.add(showNode(node, 750 + i * 400, 400))
-		}
+		// Computer to server
+		const path2 = makeBinaryPath(
+			pcServerPathRef,
+			`.${styles.computerBinary}`
+		)
+		if (path2) binary2Tl.current = path2
+		const node2Tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: '.chapter5',
+				start: `top top-=${1150}`,
+				end: `+=${400}px`,
+				scrub: true,
+				onUpdate: self => {
+					if (binary2Tl.current) {
+						if (self.direction > 0 && self.progress >= 0.95) {
+							binary2Tl.current.play()
+						} else {
+							binary2Tl.current.pause(0)
+						}
+					}
+				},
+			},
+		})
+		node2Tl.to(nodeClasses[1], {
+			autoAlpha: 1,
+			duration: 0.1,
+		})
+		node2Tl.to(nodeClasses[1], {
+			boxShadow:
+				'inset 5px 5px 10px #140025, inset -5px -5px 10px #500093',
+			duration: 1,
+		})
+		node2Tl.to(
+			`${nodeClasses[1]} .${styles.icon}`,
+			{
+				autoAlpha: 1,
+				delay: 0.2,
+				duration: 1,
+			},
+			'<'
+		)
+		// Server to database
+		const path3 = makeBinaryPath(serverDataPathRef, `.${styles.dataBinary}`)
+		if (path3) binary3Tl.current = path3
+		const node3Tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: '.chapter5',
+				start: `top top-=${1550}`,
+				end: `+=${400}px`,
+				scrub: true,
+				onUpdate: self => {
+					if (binary3Tl.current) {
+						if (self.direction > 0 && self.progress >= 0.95) {
+							binary3Tl.current.play()
+						} else {
+							binary3Tl.current.pause(0)
+						}
+					}
+				},
+			},
+		})
+		node3Tl.to(nodeClasses[2], {
+			autoAlpha: 1,
+			duration: 0.1,
+		})
+		node3Tl.to(nodeClasses[2], {
+			boxShadow:
+				'inset 5px 5px 10px #140025, inset -5px -5px 10px #500093',
+			duration: 1,
+		})
+		node3Tl.to(
+			`${nodeClasses[2]} .${styles.icon}`,
+			{
+				autoAlpha: 1,
+				delay: 0.2,
+				duration: 1,
+			},
+			'<'
+		)
+		// Server to tower
+		const node4Tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: '.chapter5',
+				start: `top top-=${1950}`,
+				end: `+=${400}px`,
+				scrub: true,
+			},
+		})
+		node4Tl.to(nodeClasses[3], {
+			autoAlpha: 1,
+			duration: 0.1,
+		})
+		node4Tl.to(nodeClasses[3], {
+			boxShadow:
+				'inset 5px 5px 10px #140025, inset -5px -5px 10px #500093',
+			duration: 1,
+		})
+		node4Tl.to(
+			`${nodeClasses[3]} .${styles.icon}`,
+			{
+				autoAlpha: 1,
+				delay: 0.2,
+				duration: 1,
+			},
+			'<'
+		)
 
-		// const delay = 400
-		// // Show computer
-		// networkTl.add(showNode(nodeClasses[0], 750, 400))
-
-		// // Show server
-		// networkTl.add(showNode(nodeClasses[1], 1500, delay))
-
-		// // Show Database
-		// networkTl.add(showNode(nodeClasses[2], 2000, delay))
-
-		// // Show tower
-		// networkTl.add(showNode(nodeClasses[3], 2500, delay))
-		// // Show satellite
-		// networkTl.add(showNode(nodeClasses[4], 3000, delay))
-		// // Show tablet
-		// networkTl.add(showNode(nodeClasses[5], 3500, delay))
-
-		const path5 = makeBinaryPath(
+		// Server to tablet path
+		const path4 = makeBinaryPath(
 			serverTabletPathRef,
-			`.${styles.tabletBinary2}`,
+			`.${styles.tabletBinary1}`,
 			{
 				duration: 3,
-				delay: 1,
 			}
 		)
-		if (path5) networkTl.add(path5)
-		const path6 = makeBinaryPath(
-			serverTabletPathRef,
-			`.${styles.tabletBinary3}`,
+		if (path4) binary4Tl.current = path4
+		// Tower to satellite
+		const node5Tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: '.chapter5',
+				start: `top top-=${2150}`,
+				end: `+=${400}px`,
+				scrub: true,
+			},
+		})
+		node5Tl.to(nodeClasses[4], {
+			autoAlpha: 1,
+			duration: 0.1,
+		})
+		node5Tl.to(nodeClasses[4], {
+			boxShadow:
+				'inset 5px 5px 10px #140025, inset -5px -5px 10px #500093',
+			duration: 1,
+		})
+		node5Tl.to(
+			`${nodeClasses[4]} .${styles.icon}`,
 			{
-				duration: 3,
-				delay: 2,
-			}
+				autoAlpha: 1,
+				delay: 0.2,
+				duration: 1,
+			},
+			'<'
 		)
-		if (path6) networkTl.add(path6)
+		// Satellite to tablet
+		const node6Tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: '.chapter5',
+				start: `top top-=${2350}`,
+				end: `+=${400}px`,
+				scrub: true,
+				onUpdate: self => {
+					if (binary4Tl.current) {
+						if (self.direction > 0 && self.progress >= 0.95) {
+							binary4Tl.current.play()
+						} else {
+							binary4Tl.current.pause(0)
+						}
+					}
+				},
+			},
+		})
+		node6Tl.to(nodeClasses[5], {
+			autoAlpha: 1,
+			duration: 0.1,
+		})
+		node6Tl.to(nodeClasses[5], {
+			boxShadow:
+				'inset 5px 5px 10px #140025, inset -5px -5px 10px #500093',
+			duration: 1,
+		})
+		node6Tl.to(
+			`${nodeClasses[5]} .${styles.icon}`,
+			{
+				autoAlpha: 1,
+				delay: 0.2,
+				duration: 1,
+			},
+			'<'
+		)
 	}, [])
 
 	return (
@@ -395,6 +551,7 @@ const Network = () => {
 							className: '',
 							order: 0,
 							icon: faServer,
+							binaryRef: null,
 						},
 						nodes.computer,
 					])}
