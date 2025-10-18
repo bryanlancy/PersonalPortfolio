f089383643883c63ccbe30d1ebfcbe3e907d5286
bryanlancy <bryanburns93@gmail.com>
2025-10-18 03:14:32 -0400

    fix hydration issue

--- src/components/sections/Intro/index.tsx
+++ src/components/sections/Intro/index.tsx
@@ -4,13 +4,16 @@ import StoryButtons from './StoryButtons'
 import styles from './Intro.module.scss'
 
 import MouseEffects from './MouseEffects'
+import { NoSsr } from '@/utils/next'
 
 export default function Intro() {
 	// await new Promise(resolve => setTimeout(resolve, 6000))
 	return (
 		<section className={styles.section}>
 			<TitleSections />
-			<MouseEffects />
+			<NoSsr>
+				<MouseEffects />
+			</NoSsr>
 			<StoryButtons />
 		</section>
 	)
